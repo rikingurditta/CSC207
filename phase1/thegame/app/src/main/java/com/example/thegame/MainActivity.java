89cacbd50dc89c91b301523e8a5dac072d48ccb9
Sean Malikin <sean.malikin@mail.utoronto.ca>
2019-10-23 15:55:49 -0400

    Added UI to MainActivity: - 3 CardViews with an imageView and TextView each for a game - The cardviews onClick leads to a method that sends to correct game activity
    Updated to latest Firebase package
    Added cardview dependency

--- phase1/thegame/app/src/main/java/com/example/thegame/MainActivity.java
+++ phase1/thegame/app/src/main/java/com/example/thegame/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.thegame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import users.IUsersService;
+import users.UsersServiceFirebaseImpl;
 
 public class MainActivity extends AppCompatActivity {
+  IUsersService userService;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    userService = UsersServiceFirebaseImpl.getInstance();
+  }
+
+  /**
+   * Go to a game based on the cardView that was pressed
+   *
+   * @param view The caller
+   */
+  public void goToGame(View view) {
+    if (userService.isUserConnected()) {
+      Class targetActivity = null;
+
+      // Check which card called me
+      CardView sendingCard = (CardView) view;
+      switch (sendingCard.getId()) {
+        case R.id.CardGame1:
+          //        targetActivity = Game1Activity.class;
+          break;
+        case R.id.CardGame2:
+          //        targetActivity = Game2Activity.class;
+          break;
+        case R.id.CardGame3:
+          //        targetActivity = Game3Activity.class;
+          break;
+        default:
+          break;
+      }
+
+      try {
+        // Try to go to selected game
+        Intent intent = new Intent(this, targetActivity);
+        startActivity(intent);
+      } catch (NullPointerException ex) {
+        Toast toast = Toast.makeText(this, "Illegal operation", Toast.LENGTH_SHORT);
+        toast.show();
+      }
+    } else {
+      startActivity(userService.initiateSignIn());
     }
+  }
 }
