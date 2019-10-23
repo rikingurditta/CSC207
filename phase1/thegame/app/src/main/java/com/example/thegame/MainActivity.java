a945265510d4cd0109f5cdf06e6308f0df0a2317
Sean Malikin <sean.malikin@mail.utoronto.ca>
2019-10-23 17:39:24 -0400

    Created ErrorHandlers module: - IErrorHandler is an interface for generic Throwable that exposes 3 methods: - - Crash which crashes the application - - Ignore which logs the error and continues running the app - - Alert which shows a message to the user, logs and continues running. This method also requires an IErrorDisplayer to be sent in order to expose message relaying methods. - IErrorDisplayer is an interface that exposes a method to display message to the user. - ExceptionErrorHandler is an implementation of IErrorHandler for the Exception class.
    Exposed global context by adding singleton extension to Application. The singleton also implements the IErrorDisplayer interface and can be used to relay error messages to user.
    
    Added example of Alert in MainActivity

--- phase1/thegame/app/src/main/java/com/example/thegame/MainActivity.java
+++ phase1/thegame/app/src/main/java/com/example/thegame/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.thegame;
 import com.group0565.engine.android.GameActivity;
 import com.group0565.math.Vector;
 
+import com.group0565.errorHandlers.ExceptionErrorHandler;
+
 public class MainActivity extends GameActivity {
-    private static final String TAG = "MainActivity";
+  private static final String TAG = "MainActivity";
 
+  public MainActivity() throws Exception {
+    super(new MainObject(null, new Vector(), false));
 
-    public MainActivity() {
-        super(new MainObject(null, new Vector(), false));
-    }
+    ExceptionErrorHandler ex = new ExceptionErrorHandler();
+    ex.Alert(TheGameApplication.getInstance(), new Exception(), "bla");
+  }
 }
