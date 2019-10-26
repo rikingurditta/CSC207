a46cd75a549e948f6bc481782513f45d4d0ac19a
Brian Diep <brian.diep@mail.utoronto.ca>
2019-10-26 12:42:44 -0400

    Changed constructors of Obstacle and ObstacleManager. Added methods addObstacle and removeObstacle to ObstacleManager.

--- phase1/thegame/app/src/main/java/com/group0565/racer/Obstacle.java
+++ phase1/thegame/app/src/main/java/com/group0565/racer/Obstacle.java
@@ -1,14 +1,14 @@
 package com.group0565.racer;
 
 import com.group0565.engine.gameobjects.GameObject;
+import com.group0565.math.Vector;
 
 abstract class Obstacle extends GameObject {
 
     String appearance;
 
-    Obstacle(String appearance) {
+    Obstacle(String appearance, GameObject parent, Vector position, boolean relative) {
+        super(parent, position, relative);
         this.appearance = appearance;
     }
-
-
 }
--- phase1/thegame/app/src/main/java/com/group0565/racer/ObstacleManager.java
+++ phase1/thegame/app/src/main/java/com/group0565/racer/ObstacleManager.java
@@ -3,18 +3,41 @@ package com.group0565.racer;
 import com.group0565.engine.gameobjects.GameObject;
 
 import java.util.ArrayList;
+import com.group0565.math.Vector;
 
 class ObstacleManager extends GameObject {
 
+    /**
+     * An ArrayList containing all the obstacles in the game.
+     */
     ArrayList<Object> obstacleList;
 
-    ObstacleManager() {
-        super();
+    /**
+     * Constructs an ObstacleManager object
+     * @param parent the parent of this ObstacleManager
+     * @param position the co-ordinates of this object in vector form
+     * @param relative boolean where true -> position vector is relative to the parent, false ->
+     *                 position vector is absolute
+     */
+
+    ObstacleManager(GameObject parent, Vector position, boolean relative) {
+        super(parent, position, relative);
         obstacleList = new ArrayList<>(1);
     }
 
+    /**
+     * Adds an Obstacle from ObstacleManager
+     * @param obstacle an Obstacle Object to be added to the manager
+     */
     void addObstacle(Obstacle obstacle) {
-
+        obstacleList.add(obstacle);
     }
 
+    /**
+     * Removes an Obstacle from ObstacleManager
+     * @param obstacle
+     */
+    void removeObstacle(Obstacle obstacle) {
+        obstacleList.remove(obstacle);
+    }
 }
