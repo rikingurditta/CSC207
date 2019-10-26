9d390a3e17c3231e60c78905f6d7ebdd72929e7d
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-10-26 11:02:35 -0400

    Added Racer class as a subclass of GameObject

---
+++ phase1/thegame/app/src/main/java/com/group0565/racer/Racer.java
@@ -0,0 +1,11 @@
+package com.group0565.racer;
+
+import com.group0565.engine.gameobjects.GameObject;
+import com.group0565.math.Vector;
+
+public class Racer extends GameObject {
+
+    Racer(GameObject parent, Vector position, boolean relative, double z) {
+        super(parent, position, relative, z);
+    }
+}
