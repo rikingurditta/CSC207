0907b9ceaf23780363259e143a95222232b6b5ab
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-10-29 18:36:49 -0400

    Added SquareObstacle class and TriangleObstacle class with constructor for future development

---
+++ phase1/thegame/app/src/main/java/com/group0565/racerGame/SquareObstacle.java
@@ -0,0 +1,11 @@
+package com.group0565.racerGame;
+
+import com.group0565.math.Vector;
+
+public class SquareObstacle extends Obstacle {
+
+    public SquareObstacle(Vector position) {
+        super(position);
+    }
+
+}
---
+++ phase1/thegame/app/src/main/java/com/group0565/racerGame/TriangleObstacle.java
@@ -0,0 +1,10 @@
+package com.group0565.racerGame;
+
+import com.group0565.math.Vector;
+
+public class TriangleObstacle extends Obstacle {
+
+    TriangleObstacle(Vector position) {
+        super(position);
+    }
+}
