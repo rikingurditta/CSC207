847b9776d34f10ab796b8b3dd28eac80251857f8
Sean Malikin <sean.malikin@mail.utoronto.ca>
2019-10-30 16:42:22 -0400

    Added Themes enum
    Added osu thumbnails

---
+++ phase1/thegame/app/src/main/java/com/group0565/theme/Themes.java
@@ -0,0 +1,37 @@
+package com.group0565.theme;
+
+import com.example.thegame.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Themes {
+    DARK(R.style.AppThemeDark),
+    LIGHT(R.style.AppThemeLight);
+
+    private static Map map = new HashMap<>();
+
+    static {
+        for (Themes Theme : Themes.values()) {
+            map.put(Theme.value, Theme);
+        }
+    }
+
+    private int value;
+
+    Themes(int value) {
+        this.value = value;
+    }
+
+    public static Themes valueOf(int Theme) {
+        return (Themes) map.get(Theme);
+    }
+
+    public static Themes getDefault() {
+        return Themes.LIGHT;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
