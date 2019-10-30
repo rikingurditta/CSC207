497f2c43d47737235488759256a5787286954283
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-10-30 13:41:44 -0400

    Added 3 Buttons to RacerGame. RacerGame implements Observer interface, moved method calls in constuctor to init().

--- phase1/thegame/app/src/main/java/com/group0565/racerGame/RacerGame.java
+++ phase1/thegame/app/src/main/java/com/group0565/racerGame/RacerGame.java
@@ -3,13 +3,32 @@ package com.group0565.racerGame;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import com.group0565.engine.gameobjects.Button;
 import com.group0565.engine.gameobjects.GameObject;
+import com.group0565.engine.interfaces.Observable;
+import com.group0565.engine.interfaces.Observer;
 import com.group0565.math.Vector;
 
-public class RacerGame extends GameObject {
+public class RacerGame extends GameObject implements Observer {
+
+    Button leftButton;
+    Button middleButton;
+    Button rightButton;
 
     RacerGame(Vector position) {
         super(position);
+    }
+
+    public void init(){
+        leftButton = new Button(new Vector(100, 1750), new Vector(150, 150), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 0), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 1));
+        middleButton = new Button(new Vector(500, 1750), new Vector(150, 150), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 0), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 1));
+        rightButton = new Button(new Vector(900, 1750), new Vector(150, 150), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 0), getEngine().getGameAssetManager().getTileSheet("Test", "Test1").getTile(0, 1));
+        this.adopt(leftButton);
+        this.adopt(middleButton);
+        this.adopt(rightButton);
+        leftButton.registerObserver(this);
+        middleButton.registerObserver(this);
+        rightButton.registerObserver(this);
         this.adopt(new Racer(new Vector(500, 100), 0).setAbsolutePosition(new Vector(500, 100)));
         spawnObstacle();
     }
@@ -34,4 +53,7 @@ public class RacerGame extends GameObject {
         canvas.drawRect(2 * canvas.getWidth() / 3 - 15, 0, 2 * canvas.getWidth() / 3 + 15, 2500, colour);
     }
 
+    public void observe(Observable observable) {
+
+    }
 }
