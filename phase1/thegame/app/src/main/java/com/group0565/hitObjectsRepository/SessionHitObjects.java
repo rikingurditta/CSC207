aeebb682c02cd6434d50e794fb962943fca0ae36
Yi Cheng Zhu <yicheng.zhu@mail.utoronto.ca>
2019-10-31 11:31:00 -0400

    Added Score count to SessionHitObject

--- phase1/thegame/app/src/main/java/com/group0565/hitObjectsRepository/SessionHitObjects.java
+++ phase1/thegame/app/src/main/java/com/group0565/hitObjectsRepository/SessionHitObjects.java
@@ -8,6 +8,15 @@ import java.util.List;
 public class SessionHitObjects {
 
     private List<HitObject> hitObjects;
+    private int score;
+    private int grade;
+    private double difficulty;
+    private String datetime;
+    private int maxCombo;
+    private int S300;
+    private int S150;
+    private int S50;
+    private int S0;
 
     public SessionHitObjects(List<HitObject> hitObjects) {
         this.hitObjects = hitObjects;
@@ -27,5 +36,77 @@ public class SessionHitObjects {
 
     public void addToList(HitObject newHitObject) {
         this.hitObjects.add(newHitObject);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public double getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(double difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public int getMaxCombo() {
+        return maxCombo;
+    }
+
+    public void setMaxCombo(int maxCombo) {
+        this.maxCombo = maxCombo;
+    }
+
+    public int getS300() {
+        return S300;
+    }
+
+    public void setS300(int s300) {
+        S300 = s300;
+    }
+
+    public int getS150() {
+        return S150;
+    }
+
+    public void setS150(int s150) {
+        S150 = s150;
+    }
+
+    public int getS50() {
+        return S50;
+    }
+
+    public void setS50(int s50) {
+        S50 = s50;
+    }
+
+    public int getS0() {
+        return S0;
+    }
+
+    public void setS0(int s0) {
+        S0 = s0;
     }
 }
