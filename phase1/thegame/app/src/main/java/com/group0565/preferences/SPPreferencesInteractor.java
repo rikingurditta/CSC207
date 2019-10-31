3d5302a0a266c3deeffb91e0f95cafbaff0e6fde
Sean Malikin <sean.malikin@mail.utoronto.ca>
2019-10-31 11:01:59 -0400

    Added the ability to add/update preference through the PreferencesInteractor

--- phase1/thegame/app/src/main/java/com/group0565/preferences/SPPreferencesInteractor.java
+++ phase1/thegame/app/src/main/java/com/group0565/preferences/SPPreferencesInteractor.java
@@ -6,6 +6,8 @@ import android.content.res.Resources;
 import com.example.thegame.R;
 import com.thegame.TheGameApplication;
 
+import java.util.List;
+
 class SPPreferencesInteractor implements IPreferenceInteractor {
   /**
    * Get the current selected theme
@@ -14,10 +16,12 @@ class SPPreferencesInteractor implements IPreferenceInteractor {
    */
   @Override
   public String getTheme() {
-    SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
     Resources resources = TheGameApplication.getInstance().getResources();
-    return sp.getString(
-        resources.getString(R.string.theme_pref_id), resources.getString(R.string.def_theme_pref));
+
+      return (String)
+              getPreference(
+                      resources.getString(R.string.theme_pref_id),
+                      resources.getString(R.string.def_theme_pref));
   }
 
   /**
@@ -27,10 +31,11 @@ class SPPreferencesInteractor implements IPreferenceInteractor {
    */
   @Override
   public String getLanguage() {
-    SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
     Resources resources = TheGameApplication.getInstance().getResources();
-    return sp.getString(
-        resources.getString(R.string.lan_pref_id), resources.getString(R.string.def_lan_pref));
+
+      return (String)
+              getPreference(
+                      resources.getString(R.string.lan_pref_id), resources.getString(R.string.def_lan_pref));
   }
 
   /**
@@ -40,9 +45,75 @@ class SPPreferencesInteractor implements IPreferenceInteractor {
    */
   @Override
   public int getVolume() {
-    SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
     Resources resources = TheGameApplication.getInstance().getResources();
-    return sp.getInt(
-        resources.getString(R.string.vol_pref_id), resources.getInteger(R.integer.def_vol_pref));
+      return (int)
+              getPreference(
+                      resources.getString(R.string.vol_pref_id),
+                      resources.getInteger(R.integer.def_vol_pref));
+  }
+
+    /**
+     * Add/Update a preference
+     *
+     * @param pref The preference to add/update
+     */
+    @Override
+    public void updatePreference(IPreference pref) {
+        SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
+        SharedPreferences.Editor prefEditor = sp.edit();
+        Object preferenceValue = pref.getPrefVal();
+        String preferenceKey = pref.getPrefName();
+
+        IPreferencePutStrategy putStrategy;
+
+        putStrategy = IPreferencePutStrategy.chooseStrategy(preferenceValue);
+
+        putStrategy.put(preferenceKey, preferenceValue, prefEditor);
+
+        prefEditor.apply();
+    }
+
+    /**
+     * Add/Update a list of preferences
+     *
+     * @param prefs The preferences to add/update
+     */
+    @Override
+    public void updatePreferences(List<IPreference> prefs) {
+        SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
+        SharedPreferences.Editor prefEditor = sp.edit();
+        for (IPreference pref : prefs) {
+
+            Object preferenceValue = pref.getPrefVal();
+            String preferenceKey = pref.getPrefName();
+
+            IPreferencePutStrategy putStrategy;
+
+            putStrategy = IPreferencePutStrategy.chooseStrategy(preferenceValue);
+
+            putStrategy.put(preferenceKey, preferenceValue, prefEditor);
+        }
+
+        prefEditor.apply();
+    }
+
+    /**
+     * Gets a preference with the given key
+     *
+     * @param prefKey      The key of the preference
+     * @param defaultValue The default value in case value does not exist
+     * @return The value of the preference
+     */
+    @Override
+    public Object getPreference(String prefKey, Object defaultValue) {
+        SharedPreferences sp = TheGameApplication.getInstance().getPreferences();
+
+        Object value = sp.getAll().get(prefKey);
+
+        if (value != null) {
+            return value;
+        }
+
+        return defaultValue;
   }
 }
