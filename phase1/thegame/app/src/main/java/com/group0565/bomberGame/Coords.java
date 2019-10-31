490c28131162b1a8acb9b649b5b24d098741a0c3
Rikin <dolllyamar@gmail.com>
2019-10-30 23:02:31 -0400

    Added Coords.random to create a random coordinate within bounds

--- phase1/thegame/app/src/main/java/com/group0565/bomberGame/Coords.java
+++ phase1/thegame/app/src/main/java/com/group0565/bomberGame/Coords.java
@@ -31,8 +31,18 @@ public class Coords {
     return new Coords(x - other.x, y - other.y);
   }
 
-  /** @return the norm of the coordinates as if . */
+  /** @return the norm of the coordinates as if it is a vector. */
   public double norm() {
     return Math.sqrt(x * x + y * y);
+  }
+
+  /** @return a coordinate with minX <= x < maxX, minY <= y < maxY. */
+  public static Coords random(int minX, int minY, int maxX, int maxY) {
+    // TODO: make this function throw an error properly instead of just asserting
+    assert minX <= maxX;
+    assert minY <= maxY;
+
+    return new Coords(
+        minX + (int) (Math.random() * (maxX - minX)), minY + (int) (Math.random() * (maxY - minY)));
   }
 }
