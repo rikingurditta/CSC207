8c2089e9a34fae3af4b99dad6e0e6a0667d8dc19
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-11-26 10:07:20 -0500

    Added GameMenu and GameOverMenu classes for future development

--- phase2/thegame/app/src/main/java/com/group0565/racerGame/GameMenu.java
+++ phase2/thegame/app/src/main/java/com/group0565/racerGame/GameMenu.java
@@ -2,5 +2,5 @@ package com.group0565.racerGame;
 
 import com.group0565.engine.gameobjects.MenuObject;
 
-public class GameOverMenu extends MenuObject {
+public class GameMenu extends MenuObject {
 }
