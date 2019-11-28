e710c6797bebf290a2eb0b1c929357bfec769638
Brian Diep <brian.diep@mail.utoronto.ca>
2019-11-28 11:04:03 -0500

    Created Lane class for extensibility purposes

---
+++ phase2/thegame/app/src/main/java/com/group0565/racer/core/Lane.java
@@ -0,0 +1,32 @@
+package com.group0565.racer.core;
+
+import android.graphics.Paint;
+
+import com.group0565.engine.gameobjects.GameObject;
+import com.group0565.engine.interfaces.Canvas;
+import com.group0565.math.Vector;
+
+import java.util.ArrayList;
+
+public class Lane extends GameObject {
+
+    private boolean racerLane;
+    private ArrayList laneContents;
+
+    Lane(Vector position, double z) {
+        super(position, z);
+        laneContents = new ArrayList();
+    }
+
+    public void setRacerLane(boolean racerLane) {
+        this.racerLane = racerLane;
+    }
+
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint colour = new Paint();
+        colour.setARGB(255, 255, 255, 255);
+
+    }
+}
