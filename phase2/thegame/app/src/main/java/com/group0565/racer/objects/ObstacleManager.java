78b58cb9c4fd6d6e2d44f9d4f30345b32a66a73a
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-11-30 17:56:39 -0500

    Began work on cleaning up memory dump

--- phase2/thegame/app/src/main/java/com/group0565/racer/objects/ObstacleManager.java
+++ phase2/thegame/app/src/main/java/com/group0565/racer/objects/ObstacleManager.java
@@ -1,14 +1,19 @@
 package com.group0565.racer.objects;
 
 import com.group0565.engine.gameobjects.GameMenu;
+import com.group0565.engine.interfaces.Observable;
+import com.group0565.engine.interfaces.ObservationEvent;
 import com.group0565.math.Vector;
 
-import static com.group0565.engine.enums.HorizontalEdge.*;
-import static com.group0565.engine.enums.VerticalEdge.*;
+import java.util.ArrayList;
 
 public class ObstacleManager extends GameMenu {
 
     private Lane lane;
+
+    public static int runCounter = 0;
+
+    private ArrayList<Obstacle> deadObstacles = new ArrayList<Obstacle>();
 
     public ObstacleManager(Vector size, Lane lane) {
         super(size);
@@ -23,24 +28,36 @@ public class ObstacleManager extends GameMenu {
         } else {
             spawnSquareObstacle();
         }
-
     }
 
     public void spawnCircleObstacle() {
-        CircleObstacle circle = new CircleObstacle(this);
-
+        Obstacle circle = new CircleObstacle(this);
+        circle.registerObserver(this::observeObstacle);
         this.adopt(circle);
         circle.setRelativePosition(new Vector(0, 0));
     }
 
     public void spawnSquareObstacle() {
-        SquareObstacle square = new SquareObstacle(this);
-
+        Obstacle square = new SquareObstacle(this);
+        square.registerObserver(this::observeObstacle);
         this.adopt(square);
         square.setRelativePosition(new Vector(0, 0));
     }
 
     public Lane getLane() {
         return lane;
+    }
+
+    public void update(long ms) {
+//        for (Obstacle obstacle: deadObstacles) {
+//            getChildren().remove(obstacle.getUUID());
+//            System.out.println(runCounter++);
+//        }
+//        deadObstacles.clear();
+    }
+
+    public void observeObstacle(Observable observable, ObservationEvent observationEvent) {
+        Obstacle obstacle = (Obstacle) observable;
+        deadObstacles.add(obstacle);
     }
 }
