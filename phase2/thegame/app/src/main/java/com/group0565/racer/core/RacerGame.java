8624fadd15ed5a4ae117f6fe41765553c8b67aba
Liam Esliger <Liam.Esliger@mail.utoronto.ca>
2019-12-01 15:55:56 -0500

    Removed RacerMainMenu class

--- phase2/thegame/app/src/main/java/com/group0565/racer/core/RacerGame.java
+++ phase2/thegame/app/src/main/java/com/group0565/racer/core/RacerGame.java
@@ -3,17 +3,12 @@ package com.group0565.racer.core;
 import com.group0565.engine.gameobjects.GameObject;
 import com.group0565.engine.interfaces.Observable;
 import com.group0565.engine.interfaces.Observer;
-import com.group0565.racer.menus.RacerMainMenu;
 
 /**
  * The Game class for Racer.
  */
 public class RacerGame extends GameObject implements Observer {
 
-  /**
-   * The game screen/menu for Racer.
-   */
-  private RacerMainMenu menu;
 
   /**
    * The engine for Racer.
@@ -30,15 +25,12 @@ public class RacerGame extends GameObject implements Observer {
    * The initialization method for RacerGame.
    */
   public void init() {
-    this.menu = new RacerMainMenu();
     this.engine =  new RacerEngine();
 
     this.engine.setEnable(true);
 
-    this.adopt(menu);
     this.adopt(engine);
 
-    menu.registerObserver(this);
     engine.registerObserver(this);
 
     super.init();
